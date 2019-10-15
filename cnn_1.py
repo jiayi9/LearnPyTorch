05657fb08f0b5907ff08cdd2b69e16b480e85096
LU Jiayi (RBCD/TEF) <Jiayi.LU2@cn.bosch.com>
2019-10-15 11:35:09 +0800

    s

--- cnn_1.py
+++ cnn_1.py
@@ -18,8 +18,22 @@ def load_dataset():
     )
     return train_loader
 
-for batch_idx, (data, target) in enumerate(load_dataset()):
+# for batch_idx, (data, target) in enumerate(load_dataset()):
     #train network
 
+data_path = "C:\LV_CHAO_IMAGE\simulation_data"
+train_dataset = torchvision.datasets.ImageFolder(
+    root=data_path,
+    transform=torchvision.transforms.ToTensor()
+)
+train_loader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=64,
+    num_workers=0,
+    shuffle=True
+)
 
+for i in train_loader:
+    print(i)
 
+train_loader
