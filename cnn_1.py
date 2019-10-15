4b5face9bd828187ea303c13b0ae9ec3dc083d05
LU Jiayi (RBCD/TEF) <Jiayi.LU2@cn.bosch.com>
2019-10-15 10:38:33 +0800

    s

---
+++ cnn_1.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+
+def load_dataset():
+    data_path = 'data/train/'
+    train_dataset = torchvision.datasets.ImageFolder(
+        root=data_path,
+        transform=torchvision.transforms.ToTensor()
+    )
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=64,
+        num_workers=0,
+        shuffle=True
+    )
+    return train_loader
+
+for batch_idx, (data, target) in enumerate(load_dataset()):
+    #train network
+
+
+
